eb7a6d8ff72091f5fdffef4479c663a272fa7464
Jocy Neto <jocygneto@gmail.com>
2022-11-18 15:35:20 -0300

    finalizando projeto

--- anotacoes.md
+++ anotacoes.md
@@ -1,12 +1,14 @@
-# Anotações importantes:
+# Anotações importantes
 
-# 0- Criando arquivo de configuração
-npm init -y
+1. Criando arquivo de configuração:
+    npm init -y
 
-# 1- Instalando o Typescript
-npm install typescript      (instalará localmente no projeto)
-npm install -g typescript   (instalará globalmente) 
+2. Instalando o Typescript:
+    - npm install typescript      (instalará localmente no projeto)
+    - npm install -g typescript   (instalará globalmente) 
 
-# 2- Compilando arquivos Typescript
-## 2.1 - Metodo 01:
-npx tsc nome_do_arquivo.ts
+3. Compilando arquivos Typescript:
+    - Metodo 01:
+        npx tsc nome_do_arquivo.ts
+
+    - Metodo 02:
--- basics.ts
+++ basics.ts
@@ -47,3 +47,95 @@ persons = [ {name: 'Jocy', age: 29}, {name: 'Max', age: 49}]
 //          Metodo 02:
 let listPersons: Person[];
 listPersons = [ {name: 'Jocy', age: 29}, {name: 'Max', age: 49}]
+
+// Funcões
+function dividir (a:number , b:number){
+    return a/b;
+}
+
+const dividir2 = (a:number, b:number) => {
+    return a/b;
+}
+
+// Generics:
+function inserirNoInicio<T>(array: T[], valor: T){
+    const novoArray = [valor, ...array];
+    return novoArray;
+}
+
+const inserirNoInicio2 = <T>(array: T[], valor: T) =>{
+    const novoArray = [valor, ...array];
+    return novoArray;
+}
+
+const pilhaInt = inserirNoInicio([1,2,3], -1);           //retornara ~~> [-1,1,2,3]
+const pilhaStr = inserirNoInicio2(['b','c','d'], 'a');   //retornara ~~> [a,b,c,d]
+
+//Classes:
+
+class Estudante{
+    
+    /* Metodo 01:
+    public nome: string;
+    public idade: number;
+    private aulas: string[];
+
+    constructor(nome: string, idade: number, aulas:string[]){
+        this.nome = nome;
+        this.idade = idade;
+        this.aulas = aulas;
+    }*/
+    
+    // Metodo 02:
+    constructor(
+        public nome: string, 
+        public idade: number, 
+        private aulas:string[]
+    ){}
+
+    adicionaAula(nomeAula: string){
+        this.aulas.push(nomeAula);
+    }
+
+    getAulas(){
+        return this.aulas.slice();
+    }
+
+
+}
+
+const estudante = new Estudante('Jocy', 29, ['.net', 'angular', 'TCC'])
+
+//Interfaces (Criando e usando)
+class Funcionario {
+    constructor(public name:string, public idade: number){}
+}
+
+class Dev extends Funcionario implements autenticavel{
+    
+    private user = 'adm';
+    private pass = '123';
+    
+    autenticar(user: string, pass: string){
+        if(this.user === user && this.pass === pass){
+            return true;
+        }
+        return false;
+    }
+
+}
+
+class Design extends Funcionario{
+
+}
+
+interface autenticavel {
+    //autenticar(user: string, pass: string)
+    autenticar: (user: string, pass: string) => void;
+}
+
+const dev = new Dev('Jocy', 29);
+const designer = new Design('Fulano', 22);
+
+dev.autenticar('adm', '123');
+
